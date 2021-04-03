0d38a6c760aaccab11cd1b205b946e45d303cd8b
Anonymous <>
2021-04-02 17:56:53 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 ASGL="antora-site-generator-lunr"
 set -eu
-set -o pipefail 
+set -o pipefail
+export DOCSEARCH_ENABLED=true
+export DOCSEARCH_ENGINE=lunr
+export NODE_PATH="$(npm --global root)"
 
 npm install --global $ASGL
 echo "Using $INPUT_SITE_SOURCES_PATH"
 cd $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
 ls -lt --reverse $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
-DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr NODE_PATH="$(npm --global root)" antora --generator $ASGL --stacktrace $INPUT_ANTORA_PLAYBOOK
+antora --generator $ASGL --stacktrace $INPUT_ANTORA_PLAYBOOK
