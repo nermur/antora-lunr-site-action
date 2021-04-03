fe3eb1ca54ced4dd0d29c5d8cdd31075499984c9
Anonymous <>
2021-04-02 18:11:15 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ export NODE_PATH="$(npm --global root)"
 npm install --global $ASGL
 echo "Using $INPUT_SITE_SOURCES_PATH"
 cd $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
-ls -lt --reverse $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
+ls -ltr $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
 antora --generator $ASGL --stacktrace $INPUT_ANTORA_PLAYBOOK
